c18658d76d5e78fc4fed1de44c694fe45792bb25
Marcin Warpechowski <warpech@gmail.com>
2013-08-02 09:31:54 +0200

    handle Location header (and Location cookie failover); send back X-Referer header; basic error handling

--- src/puppet.js
+++ src/puppet.js
@@ -12,17 +12,58 @@
     this.callback = callback;
     this.obj = null;
     this.observer = null;
-    this.xhr(remoteUrl, 'application/json', null, this.bootstrap.bind(this));
+    this.referer = readCookie("Location");
+    eraseCookie("Location");
+    this.xhr(this.remoteUrl, 'application/json', null, this.bootstrap.bind(this));
+  }
+
+  //http://www.quirksmode.org/js/cookies.html
+  function createCookie(name, value, days) {
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+
+  //http://www.quirksmode.org/js/cookies.html
+  function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  //http://www.quirksmode.org/js/cookies.html
+  function eraseCookie(name) {
+    createCookie(name, "", -1);
   }
 
   Puppet.prototype.bootstrap = function (event) {
+    this.handleResponseHeader(event.target);
     this.obj = JSON.parse(event.target.responseText);
     this.observe();
-    this.callback(this.obj);
+    if (this.callback) {
+      this.callback(this.obj);
+    }
     if (lastClickHandler) {
       document.body.removeEventListener('click', lastClickHandler);
     }
     document.body.addEventListener('click', lastClickHandler = this.clickHandler.bind(this));
+    window.addEventListener('popstate', this.historyHandler.bind(this)); //better here than in constructor, because Chrome triggers popstate on page load
+  };
+
+  Puppet.prototype.handleResponseHeader = function (xhr) {
+    var location = xhr.getResponseHeader('Location');
+    if (location) {
+      this.referer = location;
+    }
   };
 
   Puppet.prototype.observe = function () {
@@ -37,10 +78,11 @@
   };
 
   Puppet.prototype.handleLocalChange = function (patches) {
-    this.xhr(this.remoteUrl, 'application/json-patch+json', JSON.stringify(patches), this.handleRemoteChange.bind(this));
+    this.xhr(this.referer || this.remoteUrl, 'application/json-patch+json', JSON.stringify(patches), this.handleRemoteChange.bind(this));
   };
 
   Puppet.prototype.handleRemoteChange = function (event) {
+    this.handleResponseHeader(event.target);
     var patches = JSON.parse(event.target.responseText);
     this.unobserve();
     jsonpatch.apply(this.obj, patches);
@@ -54,6 +96,10 @@
     return (href.protocol == window.location.protocol && href.host == window.location.host);
   };
 
+  Puppet.prototype.changeState = function (href) {
+    this.xhr(href, 'application/json-patch+json', null, this.handleRemoteChange.bind(this));
+  };
+
   Puppet.prototype.clickHandler = function (event) {
     var target = event.target;
     if (window.PuppetExternalLink) {
@@ -63,16 +109,30 @@
     if (target.href && this.isApplicationLink(target)) {
       event.preventDefault();
       history.pushState(null, null, target.href);
-      this.xhr(target.href, 'application/json-patch+json', null, this.handleRemoteChange.bind(this));
+      this.changeState(target.href);
     }
+  };
+
+  Puppet.prototype.historyHandler = function (/*event*/) {
+    this.changeState(location.href);
   };
 
   Puppet.prototype.xhr = function (url, accept, data, callback) {
     var req = new XMLHttpRequest();
     req.addEventListener('load', callback, false);
-    req.open("GET", url, true);
+    url = url || window.location.href;
+    if (data) {
+      req.open("PATCH", url, true);
+      req.setRequestHeader('Content-Type', 'application/json-patch+json');
+    }
+    else {
+      req.open("GET", url, true);
+    }
     if (accept) {
       req.setRequestHeader('Accept', accept);
+    }
+    if (this.referer) {
+      req.setRequestHeader('X-Referer', this.referer);
     }
     req.send(data);
   };
