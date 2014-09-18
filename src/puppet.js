d9d9fc9ee7570ff3c13fb2e3e1e3182087cceabc
Marcin Warpechowski <warpech@gmail.com>
2014-09-18 12:58:38 +0200

    don't hijack history.pushState; move changeState to morphUrl

--- src/puppet.js
+++ src/puppet.js
@@ -41,18 +41,6 @@
 
     this.cancelled = false;
     this.lastRequestPromise = this.xhr(this.remoteUrl, 'application/json', null, this.bootstrap.bind(this));
-
-    /**
-     * There is on "onpushstate" event, so PuppetJS needs to wrap history.pushState to know when it is called
-     * @type {Function}
-     * @private
-     */
-    var history_pushState = window.history.pushState;
-    var that = this;
-    window.history.pushState = function (data, title, url) {
-      history_pushState.call(window.history, data, title, url);
-      that.changeState(url);
-    };
   }
 
   /**
@@ -502,6 +490,7 @@
    */
   Puppet.prototype.morphUrl = function (url) {
     history.pushState(null, null, url);
+    this.changeState(url);
   };
 
   /**
