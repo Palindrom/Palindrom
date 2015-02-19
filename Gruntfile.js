994f314fe56a36bbe94d548e2de166e22d072be4
Tomek Wytrebowicz <tomalecpub@gmail.com>
2015-02-19 15:10:26 +0100

    Add grunt-bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,8 +25,26 @@ module.exports = function(grunt) {
           'examples/**/*.html'
         ],
       },
-    }
+    },
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json', 'src/puppet.js', 'puppet.min.js'],
+        commit: true,
+        commitMessage: '%VERSION%',
+        commitFiles: ['package.json', 'bower.json', 'src/puppet.js', 'puppet.min.js'],
+        createTag: true,
+        tagName: '%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        // pushTo: 'origin',
+        globalReplace: false,
+        prereleaseName: false,
+        regExp: false
+      }
+    },
+
   });
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bump');
 };
