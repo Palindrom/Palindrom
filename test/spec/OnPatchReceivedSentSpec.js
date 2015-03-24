117ffa94586104f2826945a0a9a9cd3001357546
miyconst <const.miyang@gmail.com>
2015-03-24 16:33:49 +0300

    Spec for WS onPatchReceived & onPatchSent events
    The spec is not working. Push to git for Tomek's review.

--- test/spec/OnPatchReceivedSentSpec.js
+++ test/spec/OnPatchReceivedSentSpec.js
@@ -11,7 +11,7 @@
     });
 
     describe("init", function () {
-        it("should call callbacks onPatchSent and onPatchReceived", function (done) {
+        it("should call callbacks onPatchSent and onPatchReceived for http requests", function (done) {
             var sendStr = '{"hello": "request"}';
             var receiveStr = '{"hello": "response"}';
             var sentData, receivedData;
@@ -20,7 +20,6 @@
             this.puppet = new Puppet({
                 referer: window.location.href,
                 remoteUrl: window.location.href,
-                //useWebSocket: true,
                 onPatchSent: function (data, url) {
                     sentData = data;
                     sentUrl = url;
@@ -28,7 +27,6 @@
                 onPatchReceived: function (data, url) {
                     receivedData = data;
                     receivedUrl = url;
-                    console.log(data);
                 }
             });
 
@@ -49,6 +47,68 @@
 
                 expect(receivedData).toEqual(receiveStr);
                 expect(receivedUrl).toEqual(window.location.href);
+
+                done();
+            }, 100);
+        });
+
+        it("should call callbacks onPatchSent and onPatchReceived for socket patches", function (done) {
+            //var sendStr = '{"hello": "request"}';
+            var responsePatchStr = '[{"hello": "response"}]';
+            var modelStr = '{"hello": "request"}';
+            var sentData, receivedData;
+            var sentUrl, receivedUrl;
+            var me = this;
+
+            this.puppet = new Puppet({
+                referer: window.location.href,
+                remoteUrl: window.location.href,
+                useWebSocket: true,
+                onPatchSent: function (data, url) {
+                    sentData = data;
+                    sentUrl = url;
+                    console.log("Sent: ", data);
+                },
+                onPatchReceived: function (data, url) {
+                    receivedData = data;
+                    receivedUrl = url;
+                    console.log("Received: ", data);
+                }
+            });
+
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                "status": 200,
+                "contentType": 'application/json',
+                "responseText": modelStr,
+                responseHeaders: {
+                    "X-Location": window.location.pathname
+                }
+            });
+
+            setTimeout(function () {
+                var socket = jasmine.WebSocket.spy.calls.mostRecent().returnValue;
+
+                //me.puppet.network.send(sendStr);
+                me.puppet.obj.hello = "!!";
+                triggerMouseup();
+                setTimeout(function () {
+                    socket.onmessage({ data: responsePatchStr });
+                });
+            }, 100);
+
+            /*socket.open();
+            socket.onmessage = function (e) {
+                console.log(e);
+                socket.send(receiveStr);
+                socket.close();
+            };*/
+
+            setTimeout(function () {
+                /*expect(sentData).toEqual(sendStr);
+                expect(sentUrl).toEqual(window.location.href);
+
+                expect(receivedData).toEqual(receiveStr);
+                expect(receivedUrl).toEqual(window.location.href);*/
 
                 done();
             }, 100);
