06c83f314a801abd80ed4a0756297acb86a4ea6c
miyconst <const.miyang@gmail.com>
2015-06-17 20:49:42 +0300

    Fix angularjs demo

--- lab/angularjs/js/app.js
+++ lab/angularjs/js/app.js
@@ -4,21 +4,22 @@ var app = angular.module('MyApp', ['ngPartial', 'animateMeModule', 'xBtnModule',
  * AppController
  */
 function AppController($scope) {
-  $scope.mainFile = window.location.pathname;
+    $scope.mainFile = window.location.pathname;
 
-  var puppet = new Puppet({
-      remoteUrl: window.location.href, 
-      callback: function (obj) {
-        for (var i in obj) {
-          if (obj.hasOwnProperty(i)) {
-            $scope[i] = obj[i];
-          }
-          $scope.$apply();
+    var puppet = new PuppetDOM({
+        remoteUrl: "/PuppetJs/lab/angularjs/index.html",
+        callback: function (obj) {
+            for (var i in obj) {
+                if (obj.hasOwnProperty(i)) {
+                    $scope[i] = obj[i];
+                }
+                $scope.$apply();
+            }
         }
-      }
-  });
-  puppet.onRemoteChange = function (patches) {
-    jsonpatch.apply($scope, patches);
-    $scope.$apply();
-  };
+    });
+
+    puppet.onRemoteChange = function (patches) {
+        jsonpatch.apply($scope, patches);
+        $scope.$apply();
+    };
 }
