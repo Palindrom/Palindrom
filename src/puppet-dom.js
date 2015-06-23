67f7e26f43c4f63512154f79a6b2982aec286f79
Tomek Wytrebowicz <tomalecpub@gmail.com>
2015-06-23 17:31:06 +0200

    1.2.0

--- src/puppet-dom.js
+++ src/puppet-dom.js
@@ -1,4 +1,4 @@
-/*! puppet-dom.js version: 1.1.0
+/*! puppet-dom.js version: 1.2.0
  * (c) 2013 Joachim Wester
  * MIT license
  */
--- src/puppet.js
+++ src/puppet.js
@@ -1,4 +1,4 @@
-/*! puppet.js version: 1.1.0
+/*! puppet.js version: 1.2.0
  * (c) 2013 Joachim Wester
  * MIT license
  */
