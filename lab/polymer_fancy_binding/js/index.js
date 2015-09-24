5d5be4a3854bc089446c26d5ffc1d60b6dfe955c
Tomek Wytrebowicz <tomalecpub@gmail.com>
2015-09-24 15:26:29 +0200

    Update demos to work nice with Polymer 1.1.3

--- lab/polymer_fancy_binding/js/index.js
+++ lab/polymer_fancy_binding/js/index.js
@@ -1,53 +1,34 @@
 ﻿jasmine.Ajax.install();
 
-function getCurrentPage() {
-    var url = window.location.href;
-
-    if (url.indexOf("subpage.html") > -1) {
-        return "subpage.html";
-    } else {
-        return "index.html";
-    }
-}
-
-function getPages(current) {
-    var pages = ["index.html", "subpage.html"];
-    var result = [];
-
-    for (var i = 0; i < pages.length; i++) {
-        result.push({
-            name: pages[i],
-            selected: pages[i] == current
-        });
-    }
-
-    return result;
-}
 
 var model = window.model = {
     "_ver#c$": 0,
     "_ver#s": 0,
-    currentPage: getCurrentPage(),
-    pages: getPages(getCurrentPage()),
     text: "Polymer 1.0 demo",
     message: "There were no clicks so far.",
     clickCount$: 0,
     booleanSwitch$: false,
     checked$: false,
 
-    people:[{
-      first: "Tomek",
-      last: "Wytrębowicz"
-    }]
+    people:[
+        {
+            first: "Tomek",
+            last: "Wytrębowicz",
+            full: "Tomek Wytrębowicz"
+        },{
+            first: "Marcin",
+            last: "Warpechowski",
+            full: "Marcin Warpechowski"
+        }
+    ]
 };
 
-var stub = jasmine.Ajax.stubRequest(/(\/lab\/polymer_1\/?$|index\.html$|subpage\.html$|import\.html$|test\.json$)/);
+var stub = jasmine.Ajax.stubRequest(/(\/lab\/polymer_fancy_binding\/?$|index\.html$|import\.html$|test\.json$)/);
 stub.andReturn({
     "responseText": "Error"
 });
 
 var _old = XMLHttpRequest.prototype.send;
-var puppet = null;
 
 XMLHttpRequest.prototype.send = function (data) {
     if (/.*import\.html$/gi.test(this.url)) {
@@ -57,20 +38,21 @@ XMLHttpRequest.prototype.send = function (data) {
     } else if (this.requestHeaders["Accept"] == "application/json-patch+json") {
         var inPatches = data ? JSON.parse(data) : [];
         var outPatches = [];
-        var page = getCurrentPage();
+        jsonpatch.apply(model, inPatches);
 
         for (var i = 0; i < inPatches.length; i++) {
             var p = inPatches[i];
 
-            if (p.path == "/click$") {
+            if (p.path === "/click$") {
                 outPatches.push({ op: "replace", path: "/message", value: "The click #" + p.value + "!" });
+            }
+            if (p.path.match(/\/first|\/last/)) {
+                model.people[0].full = model.people[0].first + ' ' + model.people[0].last;
+                model.people[1].full = model.people[1].first + ' ' + model.people[1].last;
+                outPatches.push({ op: "replace", path: p.path.replace(/first|last/,"full"), value: model.people[p.path.match(/\d/)[0]].full });
             }
         }
 
-        if (puppet.obj.currentPage != page) {
-            outPatches.push({ op: "replace", path: "/currentPage", value: page });
-            outPatches.push({ op: "replace", path: "/pages", value: getPages(page) });
-        }
 
         if (outPatches) {
             model["_ver#s"]++;
@@ -82,11 +64,6 @@ XMLHttpRequest.prototype.send = function (data) {
     } else {
         stub.responseText = "Error";
     }
-
     console.info("Mock Server ", this.url, "\n request", data, "\n response", stub.status, stub.responseText);
     return _old.apply(this, [].slice.call(arguments));
 };
-
-window.addEventListener("WebComponentsReady", function () {
-    puppet = document.querySelector("puppet-client");
-});
