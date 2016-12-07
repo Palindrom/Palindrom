58e0b959fb8f2fae2d573be11b859b1f3b22bd30
Marcin Warpechowski <warpech@gmail.com>
2016-12-07 13:05:58 +0100

    2.4.0

--- puppet-dom.min.js
+++ puppet-dom.min.js
@@ -1,4 +1,4 @@
-/*! puppet-dom.js version: 2.3.0
+/*! puppet-dom.js version: 2.4.0
  * (c) 2013 Joachim Wester
  * MIT license
  */
--- puppet.min.js
+++ puppet.min.js
@@ -1,4 +1,4 @@
-/*! puppet.js version: 2.3.0
+/*! puppet.js version: 2.4.0
  * (c) 2013 Joachim Wester
  * MIT license
  */
