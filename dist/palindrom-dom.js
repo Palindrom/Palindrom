f1f6cda2d0b2873e2df669d82a9375c47a911653
Omar Alshaker <omar@omaralshaker.com>
2018-08-06 19:27:13 +0200

    5.1.2

--- dist/palindrom-dom.js
+++ dist/palindrom-dom.js
@@ -1,4 +1,4 @@
-/*! Palindrom, version: 5.1.1 */
+/*! Palindrom, version: 5.1.2 */
 var PalindromDOM =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -1824,7 +1824,7 @@ module.exports.__esModule = true;
  */
 
 /* this variable is bumped automatically when you call npm version */
-const palindromVersion = '5.1.1';
+const palindromVersion = '5.1.2';
 
 const CLIENT = 'Client';
 const SERVER = 'Server';
