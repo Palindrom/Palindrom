1622ea13b04d8c5131f8f39798b87d650d8c3f60
Tomek Wytrębowicz <tomalecpub@gmail.com>
2019-09-05 09:05:08 +0200

    test only ones that are migrated

--- test/runner.js
+++ test/runner.js
@@ -1,14 +1,14 @@
-import "./inject-mock-websocket.js";
+// import "./inject-mock-websocket.js";
 
 import './specs/palindrom.test.js';
 import './specs/palindrom.callbacks.test.js';
 import './specs/palindrom.validate.test.js';
-import './specs/palindrom.websockets.test.js';
-import './specs/onPatchSentAndReceived.test.js';
-import './specs/palindrom.errors.test.js';
-import './specs/palindrom.ignore.defineProperty.test.js';
-import './specs/palindrom.filter.test.js';
-import './specs/client/heartbeat.js';
+// import './specs/palindrom.websockets.test.js';
+// import './specs/onPatchSentAndReceived.test.js';
+// import './specs/palindrom.errors.test.js';
+// import './specs/palindrom.ignore.defineProperty.test.js';
+// import './specs/palindrom.filter.test.js';
+// import './specs/client/heartbeat.js';
 
-import './specs/dom/index.js';
-import './specs/dom/ot.js'; //fixme: this does not clean up well, must be the last one
+// import './specs/dom/index.js';
+// import './specs/dom/ot.js'; //fixme: this does not clean up well, must be the last one
